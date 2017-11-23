8f06ce5e61ca933630287f1bd332b307cd0be98a
abhineetsharma <asharm28@binghamton.edu>
2017-11-23 15:02:26 -0500

    Added flag print to the boolean message check

--- src/NodeServer.java
+++ src/NodeServer.java
@@ -11,6 +11,8 @@ public class NodeServer {
     static String nodeName;
     private static Map<String, Node.InitReplica.Replica> nodeMap;
 
+    private static boolean printFlag = true;//flag to stop print
+
     static {
         nodeMap = new TreeMap<>();
     }
@@ -35,33 +37,46 @@ public class NodeServer {
                     Node.MapMessage msg = Node.MapMessage.parseDelimitedFrom(is);
 
                     if (msg != null) {
+                        print(msg);
                         //Replica message from controller
                         if (msg.hasInitReplica()) {
+                            print("----------InitReplica Start----------");
                             for(Node.InitReplica.Replica replica : msg.getInitReplica().getAllReplicaList()){
                                 if(!replica.getName().equalsIgnoreCase(nodeName)){
                                     nodeMap.put(replica.getName(),replica);
                                 }
                             }
+                            print("----------InitReplica End----------");
                         }
                         //Read message from the controller
                         else if(msg.hasRead()){
+                            print("----------Read Start----------");
 
+                            print("----------Read End----------");
                         }
                         //Write message from the controller
                         else if(msg.hasWrite()){
+                            print("----------Write Start----------");
 
+                            print("----------Write End----------");
                         }
                         //Get Key message from coordinator
                         else if(msg.hasGetKey()){
+                            print("----------GetKey Start----------");
 
+                            print("----------GetKey End----------");
                         }
                         //Put Key message from coordinator
                         else if(msg.hasPutKeyVal()){
+                            print("----------PutKeyVal Start----------");
 
+                            print("----------PutKeyVal End----------");
                         }
                         //Read Repair Message from the coordinator
                         else if(msg.hasReadRepair()){
+                            print("----------ReadRepair Start----------");
 
+                            print("----------ReadRepair End----------");
                         }
                     }
 
@@ -71,6 +86,14 @@ public class NodeServer {
             e1.printStackTrace();
         } catch (IOException e1) {
             e1.printStackTrace();
+        }
+    }
+    private static void print(Object obj) {
+        if (printFlag) {
+            if (obj != null)
+                System.out.println(obj.toString());
+            else
+                System.out.println("null");
         }
     }
 
