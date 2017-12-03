b6512459184e9a190469932bd0a4cf26b39a6f1c
abhineetsharma <asharm28@binghamton.edu>
2017-12-03 17:26:13 -0500

    removed the unwanted function declaration property

--- src/Server.java
+++ src/Server.java
@@ -45,7 +45,7 @@ public class Server {
         allServersData = new TreeMap<>();
         keyValueDataStore = new ConcurrentSkipListMap<>();
         CoordinatorAcknowledgementLog = new ConcurrentSkipListMap<>();
-        failedWrites = new HashMap<>();
+        failedWrites = new ConcurrentSkipListMap<>();
         replicaFactor = 4;
 
         String str = "";
@@ -284,11 +284,11 @@ public class Server {
             messages.add(conflictingReplica);
             failedWrites.put(serverName, messages);
         }
-        writeToFile(failedWrites);
+        writeToFile();
         print("----Failed WriteRequest For Replica End-----");
     }
 
-    private void writeToFile(Map<String, List<ConflictingReplica>> failedWritesI) {
+    private void writeToFile() {
 
         MyCassandra.HintedHandOffBook.Builder hhfBook = MyCassandra.HintedHandOffBook.newBuilder();
 
@@ -690,7 +690,7 @@ public class Server {
             //delete the server name from failedWrites.
             if (allSent) {
                 failedWrites.remove(respawnReplica);
-                writeToFile(failedWrites);
+                writeToFile();
             }
             print("----Hinted hand off message End------");
         }
